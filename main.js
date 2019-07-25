41d4588ba06223b3ae9c4ae01b5545ba7937806b
Longern <i@longern.com>
2019-07-25 12:35:10 +0800

    Add no-new to main.js

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
   await h5native.requireAsync('ansi-to-html')
 })()
 
+// eslint-disable-next-line no-new
 new Vue({
   el: '#ssh-app',
   components: {
-    'app': httpVueLoader('src/App.vue')
+    app: httpVueLoader('src/App.vue')
   },
   vuetify: new Vuetify()
 })
